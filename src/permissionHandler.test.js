ce3e0ec057d2f6b3dd1a613d3204d22176a0346c
Fredrik Fossum <fredrik.fossum@gmail.com>
2017-06-03 19:39:16 +0200

    Update permission handler test

--- src/permissionHandler.test.js
+++ src/permissionHandler.test.js
@@ -1,7 +1,7 @@
 const PermissionHandler = require('./permissionHandler');
 
 describe('permission handler', () => {
-  it('gives permission to the server', () => {
+  test('gives permission to the server', () => {
     const serverUsername = 'server username';
     const handler = new PermissionHandler(serverUsername);
 
@@ -11,7 +11,47 @@ describe('permission handler', () => {
     expect(callback).toHaveBeenCalledWith(null, true);
   });
 
-  it('clients are not allowed to perform actions, ...for now', () => {
+  test('everyone are allowed to subscribe to events', () => {
+    const handler = new PermissionHandler();
+
+    const callback = jest.fn();
+    handler.canPerformAction(
+      'OPEN',
+      {
+        topic: 'E',
+        action: 'S',
+      },
+      callback
+    );
+
+    expect(callback).toHaveBeenCalledWith(null, true);
+  });
+
+  describe('emitted event message containing uid', () => {
+    const eventData = {
+      data: ['', 'O{"uid": "asdf"}'],
+      topic: 'E',
+      action: 'EVT',
+    };
+    test('is rejected if socket username does not match uid', () => {
+      const handler = new PermissionHandler();
+
+      const callback = jest.fn();
+      handler.canPerformAction('OPEN', eventData, callback);
+
+      expect(callback).toHaveBeenCalledWith(null, false);
+    });
+    test('is allowed if socket username matches uid', () => {
+      const handler = new PermissionHandler();
+
+      const callback = jest.fn();
+      handler.canPerformAction('asdf', eventData, callback);
+
+      expect(callback).toHaveBeenCalledWith(null, true);
+    });
+  });
+
+  test('otherwise, message is disallowed', () => {
     const handler = new PermissionHandler();
 
     const callback = jest.fn();
